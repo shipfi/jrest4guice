d0062762ebcab47e91b24b54705a54f9aeb47227
gzyangfan@gmail.com <gzyangfan@gmail.com@7b7aa470-6449-0410-8270-e1cb95179ea8>
2008-04-11 08:43:23 +0000

    添加 @Retrieve 的支持

--- trunk/JRest4Guice-dao/test/org/jrest/dao/test/jpa/RetrieveDaoTest.java
+++ trunk/JRest4Guice-dao/test/org/jrest/dao/test/jpa/RetrieveDaoTest.java
@@ -17,21 +17,21 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RetrieveDaoTest {
-
+	
 	private static RetrieveDao dao;
 	private static BookDao bookDao;
 	private static String pk;
-
+	
 	@SuppressWarnings("unchecked")
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		GuiceContext context = GuiceContext.getInstance();
 		context.addModuleProvider(
-		    new JpaGuiceModuleProvider(),
-		    (new DaoModuleProvider("org.jrest.dao.test.jpa", "org.jrest.dao.jpa"))
-		        .addActionContextProviders(new JpaContextProvider())).init();
+		        new JpaGuiceModuleProvider(),
+		        (new DaoModuleProvider("org.jrest.dao.test.jpa", "org.jrest.dao.jpa"))
+		                .addActionContextProviders(new JpaContextProvider())).init();
 		bookDao = context.getBean(BookDao.class);
-
+		
 		Book b0 = getNewBook("Book 0", 10f, 10);
 		Book b1 = getNewBook("Book 1", 10f, 20);
 		Book b2 = getNewBook("Book 2", 10f, 30);
@@ -43,7 +43,7 @@ public class RetrieveDaoTest {
 		Book b8 = getNewBook("Book 8", 20f, 90);
 		Book b9 = getNewBook("Book 9", 20f, 100);
 		bookDao.create(b0, b1, b2, b3, b4, b5, b6, b7, b8, b9);
-
+		
 		pk = b0.getId();
 		Assert.assertNotNull(b0.getId());
 		Assert.assertNotNull(b1.getId());
@@ -55,58 +55,65 @@ public class RetrieveDaoTest {
 		Assert.assertNotNull(b7.getId());
 		Assert.assertNotNull(b8.getId());
 		Assert.assertNotNull(b9.getId());
-
+		
 		dao = context.getBean(RetrieveDao.class);
 		Assert.assertNotNull(dao);
 	}
-
+	
 	@Test
 	public void testLoadByPk() {
 		Book book = dao.loadByPk(pk);
 		Assert.assertNotNull(book);
 		Assert.assertEquals(pk, book.getId());
 	}
-
+	
+	@Test
+	public void testLoadByTitle() {
+		Book book = dao.loadByTitle("Book 0");
+		Assert.assertNotNull(book);
+		Assert.assertEquals(pk, book.getId());
+	}
+	
 	@Test
 	public void testLoadByQuery() {
 		Book book = dao.loadByQuery("Book 0");
 		Assert.assertNotNull(book);
 		Assert.assertEquals(pk, book.getId());
 	}
-
+	
 	@Test
 	public void testLoadByNamedQuery() {
 		Book book = dao.loadByNamedQuery("Book 0");
 		Assert.assertNotNull(book);
 		Assert.assertEquals(pk, book.getId());
 	}
-
+	
 	@Test
 	public void testLoadModelByQuery() {
 		BookModel model = dao.loadModelByQuery(pk);
 		Assert.assertNotNull(model);
 		Assert.assertEquals(pk, model.getId());
 	}
-
+	
 	@Test
 	public void testLoadModelByNamedQuery() {
 		BookModel model = dao.loadModelByNamedQuery("Book 0");
 		Assert.assertNotNull(model);
 		Assert.assertEquals(pk, model.getId());
 	}
-
+	
 	private static Book getNewBook(String title, float price, int length) {
 		PackingInfo info = new PackingInfo("平装", "铜版纸", length);
 		List<Author> authors = new ArrayList<Author>();
 		authors.add(new Author("gzYangfan"));
-
+		
 		Book book = new Book();
 		book.setTitle(title);
 		book.setSummary("java");
 		book.setPrice(price);
 		book.setPackingInfo(info);
 		book.setAuthors(authors);
-
+		
 		return book;
 	}
 }
