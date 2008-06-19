0b5d5ae327f0aa2c088708c7f0bce819b20575fa
zhangyouqun@gmail.com <zhangyouqun@gmail.com@7b7aa470-6449-0410-8270-e1cb95179ea8>
2008-06-19 07:35:25 +0000

    已经移动到了JRest4Guice下的tools

--- trunk/Incubator/JRest4Guice-tools/dist/generate.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cls
-java -cp .;commons-collections-3.1.jar;commons-lang-2.1.jar;oro-2.0.8.jar;velocity-1.5.jar;JRest4Guice-tools-0.0.1.jar;log4j-1.2.15.jar org.jrest4guice.tools.CodeGenerator
